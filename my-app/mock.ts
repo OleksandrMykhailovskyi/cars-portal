b1adc8db1009223e80bf10df1cd4e9f4df8c31e3
Oleksandr Mykhailovskiy <aleksejmikhajlovs@gmail.com>
2024-03-26 14:11:06 +0200

    feat: add basic layout

---
+++ my-app/mock.ts
@@ -0,0 +1,107 @@
+export const mockCarDetailsData = [
+  {
+    title: "Oferta od",
+    value: "Prywatne",
+    isLink: false,
+  },
+  {
+    title: "Pokaż oferty z numerem VIN",
+    value: "Tak",
+    isLink: false,
+  },
+  {
+    title: "Marka pojazdu",
+    value: "Audi",
+    isLink: true,
+  },
+  {
+    title: "Model pojazdu",
+    value: "Q5",
+    isLink: true,
+  },
+  {
+    title: "Generacja",
+    value: "8R (2008-2016)",
+    isLink: true,
+  },
+  {
+    title: "Rok produkcji",
+    value: "2010",
+    isLink: false,
+  },
+  {
+    title: "Przebieg",
+    value: "262 421 km",
+    isLink: false,
+  },
+  {
+    title: "Pojemność skokowa",
+    value: "1 968 cm3",
+    isLink: false,
+  },
+  {
+    title: "Rodzaj paliwa",
+    value: "Diesel",
+    isLink: true,
+  },
+  {
+    title: "MOC",
+    value: "177 KM",
+    isLink: false,
+  },
+  {
+    title: "Skrzynia biegów",
+    value: "Automatyczna",
+    isLink: true,
+  },
+  {
+    title: "Napęd",
+    value: "4x4 (stały)",
+    isLink: true,
+  },
+  {
+    title: "Typ nadwozia",
+    value: "SUV",
+    isLink: true,
+  },
+  {
+    title: "Liczba drzwi",
+    value: "5",
+    isLink: false,
+  },
+  {
+    title: "Liczba miejsc",
+    value: "5",
+    isLink: false,
+  },
+  {
+    title: "Kolor",
+    value: "Czarny",
+    isLink: true,
+  },
+  {
+    title: "Rodzaj koloru",
+    value: "Metalik",
+    isLink: false,
+  },
+  {
+    title: "Zarejestrowany w Polsce",
+    value: "Tak",
+    isLink: true,
+  },
+  {
+    title: "Bezwypadkowy",
+    value: "Tak",
+    isLink: true,
+  },
+  {
+    title: "Serwisowany w ASO",
+    value: "Tak",
+    isLink: true,
+  },
+  {
+    title: "Stan",
+    value: "Używane",
+    isLink: true,
+  },
+]
