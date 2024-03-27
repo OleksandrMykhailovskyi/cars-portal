2816026ff68937bdbdffdfd2bcb51ab69ff08dd0
Oleksandr Mykhailovskiy <aleksejmikhajlovs@gmail.com>
2024-03-27 10:18:07 +0200

    feat: add footer, icons layout

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,15 @@
 import { mockCarDetailsData } from "../../mock";
 
+import ShareIcon from "@/components/icons/ShareIcon";
+import BookmarkIcon from "@/components/icons/BookmarkIcon";
+import PhoneIcon from "@/components/icons/PhoneIcon";
+import EnvelopeIcon from "@/components/icons/EnvelopeIcon";
+
 export default function Home() {
   return (
     <div className="relative">
       <header></header>
-      <main className="mx-4 divide-y flex flex-col">
+      <main className="mx-4 divide-y flex flex-col mb-20">
         <div className="my-6 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
             <h3 className="text-2xl font-bold">Audi Q5</h3>
@@ -41,11 +46,11 @@ export default function Home() {
             <p className="text-sm text-[#020309]">ID: 612622343423</p>
           </div>
           <div className="flex gap-2 items-center">
-            sd
+            <ShareIcon />
             <p className="text-[#0071CE] text-xs font-bold">Udostępnij to ogłoszenie</p>
           </div>
           <div className="flex gap-2 items-center">
-            sdf
+            <BookmarkIcon />
             <p className="text-[#0071CE] text-xs font-bold">Zgłoś naruszenie</p>
           </div>
         </div>
@@ -69,6 +74,22 @@ export default function Home() {
           </div>
         </div>
       </main>
+      <div 
+        className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-[#DCDDE0]"
+      >
+        <div className="flex gap-2 p-4">
+          <button 
+            className="flex gap-2.5 py-4 px-6 w-full items-center justify-center bg-[#C82814] text-white rounded-[4px] h-12 tracking-[0.7px] leading-4 font-medium">
+            <PhoneIcon />
+            <span>Zadzwoń</span>
+          </button>
+          <button 
+            className="flex gap-2.5 py-4 px-6 w-full items-center justify-center bg-[#0071CE] text-white rounded-[4px] h-12 tracking-[0.7px] leading-4 font-medium">
+            <EnvelopeIcon />
+            <span>Napisz</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ my-app/src/components/icons/EnvelopeIcon.tsx
@@ -0,0 +1,19 @@
+const EnvelopeIcon = () => {
+  return (
+    <svg 
+      width="1.5em" 
+      height="1.5em" 
+      viewBox="0 0 24 24" 
+      xmlns="http://www.w3.org/2000/svg" 
+      className="em93sdt2 ooa-6lnaa8"
+    >
+      <path 
+        d="M4 18V7.414l6.879 6.879h2.242L20 7.415V18H4zM18.586 6l-6.293 6.293h-.586L5.415 6h13.171zM21 4H3L2 5v14l1 1h18l1-1V5l-1-1z" 
+        fill="currentColor" 
+        fill-rule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default EnvelopeIcon;
