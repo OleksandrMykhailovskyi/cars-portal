3fc551ca0198115c38b68d4dc73ee175eb2e1630
Oleksandr Mykhailovskiy <aleksejmikhajlovs@gmail.com>
2024-03-29 11:36:28 +0200

    feat: add modal and car characteristics there

---
+++ my-app/src/components/general/Characteristics/index.tsx
@@ -0,0 +1,135 @@
+"use client"
+
+import EnvelopeIcon from "@/components/icons/EnvelopeIcon"
+import PhoneIcon from "@/components/icons/PhoneIcon"
+import { useState } from "react"
+import { useForm, SubmitHandler } from "react-hook-form"
+import { Modal } from "../Modal"
+import { Footer } from "../Footer"
+import { CharacteristicsType } from "@/services/types/types"
+
+export const defaultFormData: CharacteristicsType[] = [
+  {
+    title: "Oferta od",
+    value: "-",
+    isLink: false,
+  },
+  {
+    title: "Pokaż oferty z numerem VIN",
+    value: "-",
+    isLink: false,
+  },
+  {
+    title: "Marka pojazdu",
+    value: "-",
+    isLink: true,
+  },
+  {
+    title: "Model pojazdu",
+    value: "-",
+    isLink: true,
+  },
+  {
+    title: "Generacja",
+    value: "-",
+    isLink: true,
+  },
+  {
+    title: "Rok produkcji",
+    value: "-",
+    isLink: false,
+  },
+  {
+    title: "Przebieg",
+    value: "-",
+    isLink: false,
+  },
+  {
+    title: "Pojemność skokowa",
+    value: "-",
+    isLink: false,
+  },
+  {
+    title: "Rodzaj paliwa",
+    value: "-",
+    isLink: true,
+  },
+  {
+    title: "Skrzynia biegów",
+    value: "-",
+    isLink: true,
+  },
+  {
+    title: "Typ nadwozia",
+    value: "-",
+    isLink: true,
+  },
+  {
+    title: "Liczba drzwi",
+    value: "-",
+    isLink: false,
+  },
+  {
+    title: "Liczba miejsc",
+    value: "-",
+    isLink: false,
+  },
+  {
+    title: "Kolor",
+    value: "-",
+    isLink: true,
+  },
+  {
+    title: "Zarejestrowany w Polsce",
+    value: "-",
+    isLink: true,
+  },
+  {
+    title: "Bezwypadkowy",
+    value: "-",
+    isLink: true,
+  },
+  {
+    title: "Stan",
+    value: "-",
+    isLink: true,
+  },
+]
+
+export const Characteristics = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [formData, setFormData] = useState(defaultFormData);
+  
+  return (
+    <>
+      <div className="pt-6 mb-6 flex flex-col gap-8 mx-4">
+        <h3 className="text-2xl font-bold">Szczegóły</h3>
+        <div className="flex flex-col gap-4">
+          {formData.map(({title, value, isLink}, index) => (
+            <div key={index} className={"grid grid-cols-2 gap-x-4 text-sm"}>
+              <p className="text-[#66676C]">{title}</p>
+              {isLink ? <a href="/" className=" text-[#0071CE] underline">{value}</a> : <p className="text-[#020309]">{value}</p>}
+            </div>
+          ))}
+          <div className={"grid grid-cols-2 gap-x-4 text-[#44aa00] text-sm"}>
+            <p>Kup ten pojazd na raty</p>
+            <p>Oblicz</p>
+          </div>
+          <div className={"grid grid-cols-2 gap-x-4 text-sm mt-4"}>
+            <p className="text-[#66676C]">VIN</p>
+            <p className="text-[#020309]">Wyświetl VIN</p>
+          </div>
+        </div>
+      </div>
+      <Footer
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      />
+      <Modal 
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        setFormData={setFormData}
+      />
+    </>
+  )
+}
---
+++ my-app/src/services/types/types.ts
@@ -0,0 +1,24 @@
+export type CharacteristicsType = {
+  title: string
+  value: string
+  isLink: boolean
+}
+
+export type Inputs = {
+  offer_from: string,
+  show_offers_with_VIN: string,
+  brand: string,
+  model: string,
+  generation: string,
+  year_production: string,
+  course: string,
+  fuel_type: string,
+  transmission: string,
+  car_type: string,
+  doors_num: string,
+  sits_num: string,
+  color: string,
+  is_registered_in_poland: string,
+  absence_of_accidents: string,
+  condition: string,
+}
